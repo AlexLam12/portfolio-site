c2b050d1e8b76d9ec1ab0f8efd0e91453ca11442
AlexLam12 <Alex.e015@gmail.com>
2022-03-29 16:28:14 -0500

    image problem

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import './Portfolio.css'
+import IMG1 from '../../assets/pitchmeeting.png'
+import IMG2 from '../../assets/GameSync.png'
+import IMG3 from '../../assets/CalculatorApp.png'
 
 const Portfolio = () => {
   return (
@@ -8,28 +12,34 @@ const Portfolio = () => {
 
       <div className="container portfolio__container">
         <article className='portfolio__item'>
-          <div className="portfolio__item-image"></div>
+          <div className="portfolio__item-image">
+            <img scr={IMG1} alt="PM Logo" />
+          </div>
           <h3>Pitch Meeting</h3>
+          <small>discription</small>
           <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          <a href="https://github.com" className='btn'>Live Demo</a>
         </article>
       </div>
 
       <div className="container portfolio__container">
         <article className='portfolio__item'>
-          <div className="portfolio__item-image"></div>
+          <div className="portfolio__item-image">
+            <img scr={IMG2} alt="GS Logo" />
+          </div>
           <h3>Game Sync</h3>
+          <small>discription</small>
           <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          <a href="https://github.com" className='btn'>Live Demo</a>
         </article>
       </div>
 
       <div className="container portfolio__container">
         <article className='portfolio__item'>
-          <div className="portfolio__item-image"></div>
+          <div className="portfolio__item-image">
+            <img scr={IMG3} alt="Calculator" />
+          </div>
           <h3>My Calculator</h3>
+          <small>discription</small>
           <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          <a href="https://github.com" className='btn'>Live Demo</a>
         </article>
       </div>
     </section>
