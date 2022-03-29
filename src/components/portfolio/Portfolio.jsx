2011a1a2cdc41fb748c583e25b883cc08940539d
AlexLam12 <Alex.e015@gmail.com>
2022-03-28 19:57:51 -0500

    started portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image"></div>
+          <h3>Pitch Meeting</h3>
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+          <a href="https://github.com" className='btn'>Live Demo</a>
+        </article>
+      </div>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image"></div>
+          <h3>Game Sync</h3>
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+          <a href="https://github.com" className='btn'>Live Demo</a>
+        </article>
+      </div>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image"></div>
+          <h3>My Calculator</h3>
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+          <a href="https://github.com" className='btn'>Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
