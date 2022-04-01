5d59981082e819f91bc0b73431bd67f67a4a64ce
AlexLam12 <Alex.e015@gmail.com>
2022-03-31 22:53:24 -0500

    portfolio section complete

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,6 +4,30 @@ import IMG1 from '../../assets/pitchmeeting.png'
 import IMG2 from '../../assets/GameSync.png'
 import IMG3 from '../../assets/CalculatorApp.png'
 
+const portfolioData = [
+  {
+    id:1,
+    image: IMG2,
+    title: 'Game Sync',
+    discription: "This App is a full-stack game library app where you can cross reference to games in your library with your friend’s games to find a game you all have in common.",
+    github:'https://github.com/AlexLam12/GameSync'
+  },
+  {
+    id:2,
+    image: IMG1,
+    title: 'Pitch Project',
+    discription: "This App is a frontend app use as an artist management tool for an open mic night where you can add artists to a lineup and each artist can be select to be displayed on a separate screen.",
+    github:'https://github.com/AlexLam12/pitchmeeting'
+  },
+  {
+    id:3,
+    image: IMG3,
+    title: 'My Calculator',
+    discription: "This App is a fully functional calculator where I showcased my abilities using React.js. It relies on React hooks like useReducer and useState.",
+    github:'https://github.com/AlexLam12/My-Calculator-React'
+  }
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
@@ -11,39 +35,22 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="PM Logo" />
-          </div>
-          <h3>Pitch Meeting</h3>
-          <small>discription</small>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="GS Logo" />
-          </div>
-          <h3>Game Sync</h3>
-          <small>discription</small>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          </div>
-        </article>
-
-
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="Calculator" />
-          </div>
-          <h3>My Calculator</h3>
-          <small>discription</small>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          </div>
-        </article>
+        {
+          portfolioData.map(({id, image, title, github, discription}) => {
+            return(
+              <article key={id} className='portfolio__item'>
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <small>{discription}</small>
+              <div className="portfolio__item-cta">
+                <a href={github} className='btn' target='_blank'>Github</a>
+              </div>
+            </article>
+            )
+          })
+        }
       </div>
     </section>
   )
