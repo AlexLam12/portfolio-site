4d2b6ced5ff84bb53f63423c973a65f86469865d
AlexLam12 <Alex.e015@gmail.com>
2022-04-04 16:17:00 -0500

    Updates

---
+++ precache-manifest.2e10e3e58b54c2e9dcd1f1f4b00482e6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "573de898a9f7fe1844f7",
+    "url": "/portfolio-site/static/css/main.132a02ff.chunk.css"
+  },
+  {
+    "revision": "573de898a9f7fe1844f7",
+    "url": "/portfolio-site/static/js/main.573de898.chunk.js"
+  },
+  {
+    "revision": "594ac205583bd8141ef5",
+    "url": "/portfolio-site/static/js/1.594ac205.chunk.js"
+  },
+  {
+    "revision": "26e5d437f41e8da866a3",
+    "url": "/portfolio-site/static/js/runtime~main.26e5d437.js"
+  },
+  {
+    "revision": "37f1b009275b328816477f01bcbf3410",
+    "url": "/portfolio-site/static/media/Resume.37f1b009.pdf"
+  },
+  {
+    "revision": "291e2a6015f83571bb992ced570fb6e7",
+    "url": "/portfolio-site/static/media/ME.291e2a60.png"
+  },
+  {
+    "revision": "38d8d3347777167086717d6d2b8bf0b9",
+    "url": "/portfolio-site/static/media/me-about.38d8d334.jpg"
+  },
+  {
+    "revision": "0bd5283413b9778cf196b7fc53806b77",
+    "url": "/portfolio-site/static/media/pitchmeeting.0bd52834.png"
+  },
+  {
+    "revision": "a15442a97cdfe68bbf9a7b753ee10aed",
+    "url": "/portfolio-site/static/media/GameSync.a15442a9.png"
+  },
+  {
+    "revision": "12ad5216911dce5ecd64c6faf6f88dfb",
+    "url": "/portfolio-site/static/media/CalculatorApp.12ad5216.png"
+  },
+  {
+    "revision": "d5044d7356ad85ccb4be236225c6b023",
+    "url": "/portfolio-site/index.html"
+  }
+];
